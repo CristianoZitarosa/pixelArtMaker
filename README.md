2e3940c31a6196a53a606ab1a294c14bbf3a55fe
CristianoZitarosa <36627397+CristianoZitarosa@users.noreply.github.com>
2018-07-26 18:19:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Project: Pixel Art maker | plain JavaScript
+
+### Brief introduction
+
+This is a simple project to practice JavaScript in the Udacity nanodegree.
+In the Google Developer Challenge it was asked to be completed in jQuery as final project.
+During the nanodegree, since some of the modules were already counted as passed from the Google Challenge, I did not write this project also in plain JavaScript so I decided to do it now as personal practice.
+
+The application consists in a grid of cells (the number is defined by the user that can set height and width) to paint with the mouse.
+
+### Required functionalities:
+
+- The user defines sizes of the grid;
+- The user can choose different colors to paint;
+- There are event listeners to control the application.
+
+### Extra functionalities:
+
+- Left click to paint a cell;
+- Right click to clean a cell;
+- 2 event listener only on the grid to improve performances, instead of potentially hundreds if set on every single cell;
+- Check that the event is coming from the wanted element.
+
+### How to run the application:
+You can  run it online or download on your computer:
+
+- Visit this link.
+
+- Download the folder from git on the desktop, unzip it and open `index.html` in your browser.
+
+### License:
+
+MIT License
+
+Copyright (c) 2018 Cristiano Zitarosa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ scripts/designs.js
@@ -0,0 +1,97 @@
+/**
+*	Variables declaration
+**/
+const inputWidth = document.getElementById('inputWidth');
+const inputHeight = document.getElementById('inputHeight');
+const colorPicker = document.getElementById('colorPicker');
+const canvas = document.getElementById('canvas');
+const newGrid = document.getElementById('sizePicker');
+
+/**
+*	At loading a standard grid is built, initial values are defined in the markup.
+**/
+window.onload = () => {
+	makeGrid(inputHeight.value, inputWidth.value);
+	// addListeners(); /* no longer used */
+};
+
+/**
+*	A new grid is requested, this means a reset of the current grid but
+*		at same sizes or a reset with new sizes.
+*	The canvas is cleaned, the grid is re-built at choosen sizes.
+**/
+newGrid.onsubmit = function(e) {
+	e.preventDefault(); /* to prevent default behaviour of submit */
+	while(canvas.firstChild) { /* this cleans the table */
+		canvas.removeChild(canvas.firstChild);
+	}
+	let selectedHeight = inputHeight.value;
+	let selectedWidth = inputWidth.value;
+	makeGrid(selectedHeight, selectedWidth);
+	// addListeners(); /* no longer used */
+};
+
+/**
+*	The function that builds the grid.
+**/
+function makeGrid(x, y) {
+	for (let i = 0; i < x; i++) {
+		let tr = document.createElement('tr');
+		canvas.appendChild(tr);
+		for (let j = 0; j < y; j++) {
+			let td = document.createElement('td');
+			document.querySelector('tr:last-child').appendChild(td);
+		}
+	}
+}
+
+/**
+*	Function that paints a cell checking that e.target is a td element
+**/
+function paintCell(e) {
+	if (e.target.nodeName === 'TD') {
+		let selectedColor = colorPicker.value;
+		e.target.style.backgroundColor = selectedColor;
+	}
+}
+
+/**
+*	Function that resets a cell checking that e.target is a td element
+**/
+function resetCell(e) {
+	if (e.target.nodeName === 'TD') {
+		e.preventDefault();
+		e.target.style.backgroundColor = '#fff';
+	}
+}
+
+/**
+*	Event listeners set on the canvas.
+* This way ensures to have 2 only event listeners instead 2 for every cell
+*		in the canvas to improve performances. When a click (right or left) is
+*		detected, the linked function is called. The function verifies if the
+*		click comes from the wanted element, if true the cell is painted or cleared.
+**/
+canvas.addEventListener('click', paintCell);
+canvas.addEventListener('contextmenu', resetCell);
+
+/* no longer used */
+
+// /**
+//  *	Function that set listeners on single cells waiting for mouse clicks.
+//  **/
+// function addListeners() {
+// 	const cells = document.querySelectorAll("td"); /* array of all cells */
+// 	cells.forEach(cell => {
+// 		/* listener on each single cell to paint */
+// 		cell.onclick = function() {
+// 			let selectedColor = colorPicker.value;
+// 			this.style.backgroundColor = selectedColor;
+// 		};
+// 		cell.oncontextmenu = function(e) {
+// 			/* listener on each single cell to reset color */
+// 			e.preventDefault();
+// 			this.style.backgroundColor = "#fff";
+// 		};
+// 	});
+// }
